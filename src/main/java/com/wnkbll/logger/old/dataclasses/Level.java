d457e1a839f131cb17b4145ffb524ece547843fc
wnkbll <arzhevitin.g@gmail.com>
2024-05-08 18:05:55 +0500

    Rework version

---
+++ src/main/java/com/wnkbll/logger/old/dataclasses/Level.java
@@ -0,0 +1,19 @@
+package com.wnkbll.logger.old.dataclasses;
+
+import java.util.HashMap;
+
+public class Level {
+    public String name;
+    public Format format = new Format();
+
+    public Level(String name, String style, String foreground, String background) {
+        this.name = name;
+
+        HashMap<String, Integer> styles = Globals.getStyles();
+        HashMap<String, Integer> colors = Globals.getColors();
+
+        if (style != null) this.format.style = styles.get(style);
+        if (foreground != null) this.format.foreground = colors.get(foreground);
+        if (background != null) this.format.background = colors.get(background) + 10;
+    }
+}
